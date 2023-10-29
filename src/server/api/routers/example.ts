32b7560c7655c35333110fb483f14ac888ada039
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2023-10-29 03:17:16 -0500

    Fixed build issues

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -10,7 +10,7 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return {};
-  }),
+  // getAll: publicProcedure.query(({ ctx }) => {
+  //   return {};
+  // }),
 });
